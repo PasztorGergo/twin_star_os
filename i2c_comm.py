aef9d290b9b83902b4c73f398967f2113885db34
PasztorGergo <pasztorg05@gmail.com>
2024-07-22 10:38:27 +0200

    i2c_addr_name_💜

--- i2c_comm.py
+++ i2c_comm.py
@@ -6,13 +6,13 @@ DEVICE_BUS = 1
 
 def send_emotion(id):
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x00, [id])
+        bus.write_block_data(ESP_ADDR, 0x00, [id])
 
 def send_feature(state,feature):
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x01, [feature,state])
+        bus.write_block_data(ESP_ADDR, 0x01, [feature,state])
 
 def send_blink():
     with SMBus(DEVICE_BUS) as bus:
-        bus.write_block_data(TEENSY_ADDR, 0x02, [0x01])
+        bus.write_block_data(ESP_ADDR, 0x02, [0x01])
 
